c62eee68abcd07efba7335665b920855da93e137
thehxdev <hossein.khosravi.ce@gmail.com>
2025-08-19 17:07:27 +0330

    high-level API for handling IO with coroutines

--- main.c
+++ main.c
@@ -7,42 +7,60 @@
 // helper macro to pass a non-pointer argument
 #define _(v) ((void*)(v))
 
-static int data = 0;
+#define BUFFER_SIZE 1024
 
-void server(void) {
-	volatile int i;
-	for (i = 0; i < 10; ++i)
+void echo_routine(int clientfd) {
+	int status;
+	ssize_t nread, nwritten;
+	unsigned char buf[BUFFER_SIZE];
+
+	while (1) {
+		status = jv_wait_io(clientfd, JV_READ);
+		if (!status) {
+			// handle IO error
+		}
+		nread = read(clientfd, buf, BUFFER_SIZE);
+		if (nread <= 0)
+			break;
+
+		status = jv_wait_io(clientfd, JV_WRITE);
+		if (!status) {
+			// hanlde IO error
+		}
+		nwritten = write(clientfd, buf, nread);
+		if (nwritten == -1 || nwritten != nread) {
+			// handle write error
+		}
+
 		jv_yield();
-	data = i;
+	}
 }
 
-void reader(void) {
-poll:
-	jv_yield();
-	if (data == 0) {
-		puts("no data provided by server, yielding...");
-		goto poll;
-	}
-	printf("got data: %d\n", data);
-}
+void __main(void) {
+	int serverfd, clientfd, status;
 
-void counter(int n) {
-	int i;
-	for (i = 0; i < n; ++i) {
-		printf("%d\n", i);
+	// socket, bind, listen, etc.
+
+	while (running) {
+		status = jv_wait_io(serverfd, JV_READ);
+		if (!status) {
+			// handle io error
+		}
+		clientfd = accept(serverfd, NULL, NULL);
+		if (clientfd == -1) {
+			perror("accept");
+			return;
+		}
+		jv_run(echo_routine, jv_args( _(clientfd) ));
 		jv_yield();
 	}
-	printf("counter %d end\n", n);
 }
 
 int main(void) {
 	jv_init();
 
-	// register coroutines
-	jv_run(server, NULL);
-	jv_run(reader, NULL);
-	jv_run(counter, jv_args( _(10) ));
-
+	jv_run(__main, NULL);
+	
 	while (jv_has_active())
 		jv_yield();
 
